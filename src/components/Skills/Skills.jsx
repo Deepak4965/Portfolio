11289f33f51f4c2e23ca57f5e2ddf25b1cae7afa
Deepak <deepak.mn2004@gmail.com>
2025-07-11 01:02:51 -0800

    Update Skills.jsx

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,21 +1,63 @@
-import React from 'react';
+
 import Card from '../Card/Card';
 import './Skill.css';
 import './SkillResponsive.css';
 import frontend from '../../assets/Frontend.png';
 import react from '../../assets/React.png';
 import Tailwind from '../../assets/Tailwind.png';
+import {useGSAP} from '@gsap/react'
+import gsap from 'gsap'
+import {ScrollTrigger} from 'gsap/all'
+gsap.registerPlugin(ScrollTrigger)
 
 const Skills = () => {
+
+// Animation
+
+  useGSAP(()=>{
+    gsap.from("#heading",{
+            y:100,
+            duration:1,
+            opacity:0,
+            stagger:1,
+            scrollTrigger:{
+                trigger:"#heading",
+                scroll:"body",
+                scrub:2,
+                 
+                start:"top 80%",
+                end:"top 30%"
+            }
+        })
+         gsap.from("#slider",{
+            y:100,
+            duration:1,
+            opacity:0,
+            stagger:1,
+            scrollTrigger:{
+                trigger:"#slider",
+                scroll:"body",
+                scrub:2,
+                start:"top 80%",
+                end:"top 30%"
+            }
+        })
+  })
   return (
     <div id="Skills" className="px-4 py-10 sm:px-8 md:px-16 lg:px-24 cursor-pointer">
-      <h1 className=" text-3xl sm:text-4xl md:text-5xl font-bold mb-10 text-center"id='para'>
+      <h1 className=" text-3xl sm:text-4xl md:text-5xl font-bold mb-10 text-center"id='heading'>
         Skills
       </h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-20 sm:gap-20 md:gap-120 justifybetween items-center">
-        <Card className="Frontend" image={frontend} />
-        <Card className="React" image={react} />
-        <Card className="Tailwind" image={Tailwind} />
+
+      {/* card section */}
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3
+      py-5 lg:grid-cols-5 gap-20 sm:gap-20 md:gap-120 justify-between items-center  md:mt-[-15px] " id='title'>
+        <Card title='HTML CSS JS' image={frontend} />
+        <Card title='React'  image={react}/>
+        <Card title='Tailwind Css' image={Tailwind} />
+
+       
       </div>
     </div>
   );
